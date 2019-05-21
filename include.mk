cbc65a3b0bb3cec64adb2f2a730733e010e39797
Alexander Ben Nasrallah <me@abn.sh>
2019-05-21 12:07:43 +0200

    Verwende Variabeln in Makefile

--- include.mk
+++ include.mk
@@ -1,10 +1,13 @@
 .POSIX:
 .SUFFIXES: .tex .pdf
 
+TEX=pdflatex
+TEXFLAGS=--interaction errorstop-mode
+
 .tex.pdf:
-	pdflatex $*
-	if grep -q 'Please (re)run Biber on the file:' $*.log; then biber $*; pdflatex $*; fi
-	if grep -q 'LaTeX Warning: There were undefined references.' $*.log; then pdflatex $*; fi
+	$(TEX) $(TEXFLAGS) $*
+	if grep -q 'Please (re)run Biber on the file:' $*.log; then biber $*; $(TEX) $(TEXFLAGS) $*; fi
+	if grep -q 'LaTeX Warning: There were undefined references.' $*.log; then $(TEX) $(TEXFLAGS) $*; fi
 
 TEXOBJ = \
 	*.aux \
